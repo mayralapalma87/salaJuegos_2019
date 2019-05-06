91b5c813124960ed0dd806e237dff437e92453f0
MayraLapalma <mayralapalma87@hotmail.com>
2019-05-06 01:01:01 -0300

    se cambian estilos de listas

--- src/app/ruteando/ruteando.module.ts
+++ src/app/ruteando/ruteando.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 // importo del module principal
 import { RouterModule, Routes } from '@angular/router';
-import { AdivinaElNumeroComponent } from '../componentes/adivina-el-numero/adivina-el-numero.component';
-import { ListadoDeResultadosComponent } from '../componentes/listado-de-resultados/listado-de-resultados.component';
+
 import { LoginComponent } from '../componentes/login/login.component';
 import { ErrorComponent } from '../componentes/error/error.component';
 import { PrincipalComponent } from '../componentes/principal/principal.component';
 import { AgilidadAritmeticaComponent } from '../componentes/agilidad-aritmetica/agilidad-aritmetica.component';
 import { MenuComponent } from '../componentes/menu/menu.component';
-import { AdivinaMasListadoComponent } from '../componentes/adivina-mas-listado/adivina-mas-listado.component';
-import { AgilidadMasListadoComponent } from '../componentes/agilidad-mas-listado/agilidad-mas-listado.component';
+
 import { ListadoComponent } from '../componentes/listado/listado.component';
 import { JuegosComponent } from '../componentes/juegos/juegos.component';
 import { RegistroComponent } from '../componentes/registro/registro.component';
@@ -20,6 +18,15 @@ import { QuienSoyComponent } from '../componentes/quien-soy/quien-soy.component'
 import { ListadoDePaisesComponent } from '../componentes/listado-de-paises/listado-de-paises.component';
 import { MapaDeGoogleComponent } from '../componentes/mapa-de-google/mapa-de-google.component';
 import { JugadoresListadoComponent } from '../componentes/jugadores-listado/jugadores-listado.component';
+
+import { AdivinaMasListadoComponent } from '../componentes/adivina-mas-listado/adivina-mas-listado.component';
+import { AgilidadMasListadoComponent } from '../componentes/agilidad-mas-listado/agilidad-mas-listado.component';
+import { AdivinaElNumeroComponent } from '../componentes/adivina-el-numero/adivina-el-numero.component';
+import { ListadoDeResultadosComponent } from '../componentes/listado-de-resultados/listado-de-resultados.component';
+import { TatetiComponent } from '../componentes/tateti/tateti.component';
+import { AnagramaComponent } from '../componentes/anagrama/anagrama.component';
+import { MementeComponent } from '../componentes/Memente/Memente.component';
+import { PriedraPapelTijerasComponent } from '../componentes/PriedraPapelTijeras/PriedraPapelTijeras.component';
 
 
 // declaro donde quiero que se dirija
@@ -37,15 +44,16 @@ const MiRuteo = [
 { path: 'Juegos' ,
 component: JuegosComponent ,
 children:
-     [{path: '' , component: MenuCardComponent},
-     {path: 'Adivina' , component: AdivinaElNumeroComponent},
-      {path: 'AdivinaMasListado' , component: AdivinaMasListadoComponent},
-      {path: 'Agilidad' , component: AgilidadAritmeticaComponent},
-      {path: 'AgilidadaMasListado' , component: AgilidadMasListadoComponent},
-      // {path: 'Anagrama' , component: AnagramaComponent},
-      // {path: 'PriedraPapelTijeras' , component: PriedraPapelTijerasComponent},
-      // {path: 'Tateti' , component: TatetiComponent}
-     ]
+    [{path: '' , component: MenuCardComponent},
+    {path: 'Adivina' , component: AdivinaElNumeroComponent},
+    {path: 'AdivinaMasListado' , component: AdivinaMasListadoComponent},
+    {path: 'Agilidad' , component: AgilidadAritmeticaComponent},
+    {path: 'AgilidadaMasListado' , component: AgilidadMasListadoComponent},
+    {path: 'Anagrama' , component: AnagramaComponent},
+    {path: 'PriedraPapelTijeras' , component: PriedraPapelTijerasComponent},
+    {path: 'Tateti' , component: TatetiComponent},
+    {path: 'Memente' , component: MementeComponent},
+    ]
 },
 {path: '**' , component: ErrorComponent},
 {path: 'error' , component: ErrorComponent}];
