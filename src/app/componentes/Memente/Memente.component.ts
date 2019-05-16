a6843b1fa9a7732f4d7f512658aae48bed44cdfc
MayraLapalma <mayralapalma87@hotmail.com>
2019-05-16 18:19:42 -0300

    memes

--- src/app/componentes/Memente/Memente.component.ts
+++ src/app/componentes/Memente/Memente.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
+import { timeout } from 'rxjs/operators';
 
 @Component({
   selector: 'app-Memente',
@@ -6,7 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./Memente.component.css']
 })
 export class MementeComponent implements OnInit {
-  constructor() { }
+  intentos = 0;
+  jugada1 = '';
+  jugada2 = '';
+  identificadorJ1 = '';
+  identificadorJ2 = '';
+
+  constructor() {
+  this.intentos = 0;
+  this.jugada1 = '';
+  this.jugada2 = '';
+  this.identificadorJ1 = '';
+  this.identificadorJ2 = '';
+  }
   memes = new Array(
     {nombre: '1', seleccion: false}, {nombre: '2', seleccion: false},
     {nombre: '3', seleccion: false}, {nombre: '4', seleccion: false},
@@ -17,13 +31,7 @@ export class MementeComponent implements OnInit {
     {nombre: '5', seleccion: false}, {nombre: '6', seleccion: false},
     {nombre: '7', seleccion: false}, {nombre: '8', seleccion: false} );
 
-  intentos = 0;
-  jugada1 = '';
-  jugada2 = '';
-  identificadorJ1 = '';
-  identificadorJ2 = '';
-
-  colorCambio({ posicion, color, contenido }) {
+    colorCambio({ posicion, color, contenido }) {
     document.getElementById(posicion.toString()).style.backgroundColor = color;
     document.getElementById(posicion.toString()).innerHTML = contenido;
   }
@@ -39,49 +47,50 @@ export class MementeComponent implements OnInit {
     dato.style.opacity = '1';
     this.memes.sort(function() {return Math.random() - 0.5});
     for ( let i = 0 ; i < 16 ; i++ ) {
-      let carta = this.memes[i].nombre;
+      let meme = this.memes[i].nombre;
       let dato = document.getElementById( i.toString() );
-      dato.dataset.valor = carta;
+      dato.dataset.valor = meme;
     }
   }
   resetearJuego() {
     this.memes.sort(function() {return Math.random() - 0.5});
     for ( let i = 0 ; i < 16 ; i++ ) {
-      let carta = this.memes[i].nombre;
+      let meme = this.memes[i].nombre;
       let dato = document.getElementById( i.toString() );
-      dato.dataset.valor = carta;
-      this.colorCambio({ posicion: i, color: 'black', contenido: '?' });
+      dato.dataset.valor = meme;
+      this.colorCambio({ posicion: i, color: 'rgb(0, 0, 0, 0.8)', contenido: '?' });
     }
+    //"<img src='../assets/imagenes/Memes/dino.jpg'>"
   }
 
   girarMeme() {
+
     var evento = window.event.target as unknown as HTMLInputElement;
 
     this.jugada2 = evento.dataset.valor;
     this.identificadorJ2 = evento.id;
-    if( this.jugada1 !== '' ) {
+    if ( this.jugada1 !== '' ) {
 // tslint:disable-next-line: max-line-length
-      if( this.jugada1 === this.jugada2 && this.identificadorJ1 !== this.identificadorJ2 && this.memes[parseInt(this.identificadorJ2)].seleccion != true &&               this.memes[parseInt(this.identificadorJ1)].seleccion != true) {
+      if ( this.jugada1 === this.jugada2 && this.identificadorJ1 !== this.identificadorJ2 && this.memes[parseInt(this.identificadorJ2)].seleccion !== true && this.memes[parseInt(this.identificadorJ1)].seleccion !== true) {
 
         this.memes[parseInt(this.identificadorJ1)].seleccion = true;
         this.memes[parseInt(this.identificadorJ2)].seleccion = true;
-
-        this.colorCambio({ posicion: this.identificadorJ2, color: 'blue', contenido: this.jugada2 });
+        this.colorCambio({ posicion: this.identificadorJ2, color: '#FF5E18', contenido: this.jugada2 });
         this.vaciar();
         this.comprobar();
-      } else if(this.identificadorJ1 !== this.identificadorJ2){
-        let self = this;
-        setTimeout(function(){
-          this.colorCambio(self.identificadorJ1, 'black', '?'),
-          this.colorCambio(self.identificadorJ2, 'black', '?'),
+      } else if (this.identificadorJ1 !== this.identificadorJ2) {
+        const seconds = interval(1000);
+        setTimeout(() => {
+          this.colorCambio({posicion: this.identificadorJ1, color: 'rgb(0, 0, 0, 0.8)', contenido: "?"})
+          this.colorCambio({posicion: this.identificadorJ2, color: 'rgb(0, 0, 0, 0.8)', contenido: "?"})
           this.vaciar()
-        },200);
+        }, 500);
 
-        this.colorCambio({ posicion: this.identificadorJ2, color: 'blue', contenido: this.jugada2 });
+        this.colorCambio({ posicion: this.identificadorJ2, color: '#FF5E18', contenido: this.jugada2 });
       }
-    } else if(this.jugada2 !== 'valor') {
+    } else if (this.jugada2 !== 'valor') {
 
-      this.colorCambio({ posicion: this.identificadorJ2, color: 'blue', contenido: this.jugada2 });
+      this.colorCambio({ posicion: this.identificadorJ2, color: '#FF5E18', contenido: this.jugada2 });
 
       this.jugada1 = this.jugada2;
       this.identificadorJ1 = this.identificadorJ2;
@@ -90,13 +99,13 @@ export class MementeComponent implements OnInit {
 
   comprobar() {
     let aciertos = 0;
-    for( let i = 0 ; i < 16 ; i++ ){
+    for ( let i = 0 ; i < 16 ; i++ ) {
       if ( this.memes[i].seleccion === true ) {
         aciertos ++;
       }
     }
-    if(aciertos === 16){
-      document.getElementById('juego').innerHTML = 'GANASTE';
+    if (aciertos === 16) {
+      document.getElementById('ganaste').innerHTML = 'GANASTE';
     }
   }
 
